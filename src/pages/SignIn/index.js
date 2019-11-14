11ef2a22fae44a9ddacc3145e7b6fd01a5a4ca7a
Luis Geniole <luis.geniole@gmail.com>
2019-11-14 19:55:53 -0300

    add schemaValidation w/Yup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um email válido!')
+    .required('O email é obrigatório!'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 characteres!')
+    .required('A senha é obrigatório!'),
+});
 
 export default function SignIn() {
   const handleSubmit = data => {
@@ -13,7 +23,7 @@ export default function SignIn() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu email" />
         <Input name="password" type="password" placeholder="Sua senha..." />
 
